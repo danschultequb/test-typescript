789293eca75057d855c8bc8e1ccb1f73e6ebd896
Dan Schulte <dan.schulte.qub@live.com>
2024-06-02 11:56:40 -0400

    Add code and tests

---
+++ tests/testSkipTests.ts
@@ -0,0 +1,68 @@
+import { Test, TestRunner, TestSkip } from "../sources";
+import { MochaTestRunner } from "./mochaTestRunner";
+
+export function test(runner: TestRunner): void
+{
+    runner.testFile("testSkip.ts", () =>
+    {
+        runner.testFunction("create(boolean|undefined,string|undefined)", () =>
+        {
+            runner.test("with no arguments", (test: Test) =>
+            {
+                const skip: TestSkip = TestSkip.create();
+                test.assertNotUndefinedAndNotNull(skip);
+                test.assertTrue(skip.getShouldSkip());
+                test.assertEqual(skip.getMessage(), "");
+            });
+
+            runner.test("with null shouldSkip", (test: Test) =>
+            {
+                const skip: TestSkip = TestSkip.create(null!);
+                test.assertNotUndefinedAndNotNull(skip);
+                test.assertTrue(skip.getShouldSkip());
+                test.assertEqual(skip.getMessage(), "");
+            });
+
+            runner.test("with false shouldSkip", (test: Test) =>
+            {
+                const skip: TestSkip = TestSkip.create(false);
+                test.assertNotUndefinedAndNotNull(skip);
+                test.assertFalse(skip.getShouldSkip());
+                test.assertEqual(skip.getMessage(), "");
+            });
+
+            runner.test("with true shouldSkip", (test: Test) =>
+            {
+                const skip: TestSkip = TestSkip.create(true);
+                test.assertNotUndefinedAndNotNull(skip);
+                test.assertTrue(skip.getShouldSkip());
+                test.assertEqual(skip.getMessage(), "");
+            });
+
+            runner.test("with true shouldSkip and null message", (test: Test) =>
+            {
+                const skip: TestSkip = TestSkip.create(true, "");
+                test.assertNotUndefinedAndNotNull(skip);
+                test.assertTrue(skip.getShouldSkip());
+                test.assertEqual(skip.getMessage(), "");
+            });
+
+            runner.test("with true shouldSkip and empty message", (test: Test) =>
+            {
+                const skip: TestSkip = TestSkip.create(true, "");
+                test.assertNotUndefinedAndNotNull(skip);
+                test.assertTrue(skip.getShouldSkip());
+                test.assertEqual(skip.getMessage(), "");
+            });
+
+            runner.test("with true shouldSkip and non-empty message", (test: Test) =>
+            {
+                const skip: TestSkip = TestSkip.create(true, "hello");
+                test.assertNotUndefinedAndNotNull(skip);
+                test.assertTrue(skip.getShouldSkip());
+                test.assertEqual(skip.getMessage(), "hello");
+            });
+        });
+    });
+}
+test(MochaTestRunner.create());
